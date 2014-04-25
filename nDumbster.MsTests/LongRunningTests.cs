cc7b057b224027a84adaebb21b46146e960452f8
Eden Ridgway <eden.ridgway@gmail.com>
2014-04-25 21:10:28 +0100

    Added MsTest to try and replicate long running issue.

---
+++ nDumbster.MsTests/LongRunningTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nDumbster.Smtp;
+
+namespace nDumbster.MsTests
+{
+    [TestClass]
+    public class LongRunningTests
+    {
+        [TestMethod]
+        public void ShortIterationTest()
+        {
+            var server = SmtpSetup.SmtpServer;
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                server.ClearReceivedEmail();
+
+                using (var smtp = new SmtpClient { Host = "localhost", Port = 25 })
+                {
+                    var msg = new MailMessage
+                        {
+                            Body = "Email: " + i, 
+                            Subject = "Subject: " + i, 
+                            From = new MailAddress("somebody@foo.com")
+                        };
+
+                    msg.To.Add(new MailAddress("everybody@bar.com"));
+                    smtp.Send(msg);
+                }
+
+                int waitLoopCount = 0;
+
+                while (SmtpSetup.SmtpServer.ReceivedEmailCount == 0 && waitLoopCount < 100)
+                {
+                    waitLoopCount++;
+                    Thread.Sleep(300);
+                }
+
+                var email = server.ReceivedEmail.First();
+
+                Assert.AreEqual("Email: " + i, email.Body);
+            }
+        }
+
+        [TestMethod]
+        public void LongIterationTest()
+        {
+            var server = SmtpSetup.SmtpServer;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                server.ClearReceivedEmail();
+
+                using (var smtp = new SmtpClient { Host = "localhost", Port = 25 })
+                {
+                    var msg = new MailMessage
+                    {
+                        Body = "Email: " + i,
+                        Subject = "Subject: " + i,
+                        From = new MailAddress("somebody@foo.com")
+                    };
+
+                    msg.To.Add(new MailAddress("everybody@bar.com"));
+                    smtp.Send(msg);
+                }
+
+                Thread.Sleep(100);
+
+                int waitLoopCount = 0;
+
+                while (SmtpSetup.SmtpServer.ReceivedEmailCount == 0 && waitLoopCount < 100)
+                {
+                    waitLoopCount++;
+                    Thread.Sleep(300);
+                }
+
+                var email = server.ReceivedEmail.First();
+
+                Assert.AreEqual("Email: " + i, email.Body);
+            }
+        }
+    }
+}
---
+++ nDumbster.MsTests/SmtpSetup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nDumbster.Smtp;
+
+namespace nDumbster.MsTests
+{
+    [TestClass]
+    public class SmtpSetup
+    {
+        public static SimpleSmtpServer SmtpServer { get; set; }
+
+        [AssemblyInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            SmtpServer = SimpleSmtpServer.Start(25);
+        }
+
+        [AssemblyCleanup]
+        public static void TearDown()
+        {
+            SmtpServer.Stop();
+        }
+    }
+}
