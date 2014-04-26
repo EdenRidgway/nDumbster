e6c75648f4a44cfff9cc69bc4b23021ee7ffdb9d
Eden Ridgway <eden.ridgway@gmail.com>
2014-04-26 08:47:34 +0100

    Added another mstest scenario to try and reproduce the issue experienced by Jezz

--- nDumbster.MsTests/LongRunningTests.cs
+++ nDumbster.MsTests/LongRunningTests.cs
@@ -83,5 +83,44 @@ namespace nDumbster.MsTests
                 Assert.AreEqual("Email: " + i, email.Body);
             }
         }
+
+
+        [TestMethod]
+        public void LongWaitInBetweenMessagesTest()
+        {
+            var server = SmtpSetup.SmtpServer;
+
+            for (int i = 0; i < 3; i++)
+            {
+                server.ClearReceivedEmail();
+
+                using (var smtp = new SmtpClient { Host = "localhost", Port = 25 })
+                {
+                    var msg = new MailMessage
+                    {
+                        Body = "Email: " + i,
+                        Subject = "Subject: " + i,
+                        From = new MailAddress("somebody@foo.com")
+                    };
+
+                    msg.To.Add(new MailAddress("everybody@bar.com"));
+                    smtp.Send(msg);
+                }
+
+                int waitLoopCount = 0;
+
+                while (SmtpSetup.SmtpServer.ReceivedEmailCount == 0 && waitLoopCount < 100)
+                {
+                    waitLoopCount++;
+                    Thread.Sleep(300);
+                }
+
+                var email = server.ReceivedEmail.First();
+
+                Assert.AreEqual("Email: " + i, email.Body);
+
+                Thread.Sleep(TimeSpan.FromMinutes(2));
+            }
+        }
     }
 }
